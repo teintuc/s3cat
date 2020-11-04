5140eed8219c9cb50e4c7ef2488bbfc13ea0740a
Charles Teinturier <charles@daysofwonder.com>
2020-11-04 21:32:11 +0100

    Add the possiblity to save in a file

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/teintuc/s3cat
 
 go 1.13
 
-require github.com/aws/aws-sdk-go v1.29.7 // indirect
+require github.com/aws/aws-sdk-go v1.29.7
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"flag"
+	"fmt"
 	"io"
 	"log"
 	"net/url"
@@ -15,10 +16,37 @@ import (
 )
 
 var s3key string
+var out string
 
 func init() {
 	flag.StringVar(&s3key, "key", "", "s3 key") // s3://bucket/file
+	flag.StringVar(&out, "out", "", "(optional) output file. Stdout by default")
 	flag.Parse()
+}
+
+func writeToFile(buff *aws.WriteAtBuffer, outPath string) {
+	outrsc, err := os.Create(out)
+	if err != nil {
+		log.Fatalf("Dest file: %v", err)
+	}
+	defer outrsc.Close()
+
+	n2, err := outrsc.Write(buff.Bytes())
+	if err != nil {
+		log.Fatalf("%v", err)
+	}
+	fmt.Printf("wrote %d bytes\n", n2)
+}
+
+func writeToStdout(buff *aws.WriteAtBuffer) {
+	// Print directly the s3 file
+	bytes, err := io.Copy(os.Stdout, bytes.NewReader(buff.Bytes()))
+	if err != nil {
+		log.Fatalf("Unable to copy item %v", err)
+	}
+	if bytes <= 0 {
+		log.Fatalf("Copied 0 bytes")
+	}
 }
 
 func main() {
@@ -45,13 +73,11 @@ func main() {
 		log.Fatalf("Received 0 bytes")
 	}
 
-	// Print directly the s3 file
-	bytes, err := io.Copy(os.Stdout, bytes.NewReader(buff.Bytes()))
-	if err != nil {
-		log.Fatalf("Unable to copy item %v", err)
+	// If we have an output file we write to it.
+	// Otherwise we write to stdout.
+	if len(out) > 0 {
+		writeToFile(buff, out)
+	} else {
+		writeToStdout(buff)
 	}
-	if bytes <= 0 {
-		log.Fatalf("Copied 0 bytes")
-	}
-
 }
